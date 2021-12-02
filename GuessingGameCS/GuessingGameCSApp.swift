0316e72898301b1bf0e80fed99124c3b914c449a
lcs-jdai <jdai@lcs.on.ca>
2021-12-02 09:09:27 -0500

    Initial Commit

---
+++ GuessingGameCS/GuessingGameCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameCSApp.swift
+//  GuessingGameCS
+//
+//  Created by Jerry Dai on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
